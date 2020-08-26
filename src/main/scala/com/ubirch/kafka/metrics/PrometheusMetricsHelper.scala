d7392b35a11ce15a4e4ec49cfca00f98039e80bf
sharkgua <benoit.george@ubirch.com>
2020-08-26 10:07:01 +0200

    Adding support for JMX metrics

--- src/main/scala/com/ubirch/kafka/metrics/PrometheusMetricsHelper.scala
+++ src/main/scala/com/ubirch/kafka/metrics/PrometheusMetricsHelper.scala
@@ -1,10 +1,13 @@
 package com.ubirch.kafka.metrics
 
-import java.net.BindException
+import java.net.{ BindException, InetSocketAddress }
 
 import com.typesafe.scalalogging.LazyLogging
 import io.prometheus.client.exporter.HTTPServer
-import io.prometheus.client.hotspot.DefaultExports
+import io.prometheus.client.hotspot.{ BufferPoolsExports, ClassLoadingExports, GarbageCollectorExports, MemoryAllocationExports, MemoryPoolsExports, StandardExports, ThreadExports, VersionInfoExports }
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.jmx.JmxCollector
+import javax.management.MalformedObjectNameException
 
 import scala.annotation.tailrec
 
@@ -13,12 +16,12 @@ object PrometheusMetricsHelper extends LazyLogging {
   final private val maxAttempts = 3
 
   def create(port: Int): HTTPServer = {
-    DefaultExports.initialize()
+    val customCollectionRegistry = createCustomCollectorRegistry
 
     @tailrec
     def go(attempts: Int, port: Int): HTTPServer = {
       try {
-        new HTTPServer(port)
+        new HTTPServer(new InetSocketAddress(port), customCollectionRegistry)
       } catch {
         case e: BindException =>
           val newPort = port + new scala.util.Random().nextInt(50)
@@ -34,6 +37,33 @@ object PrometheusMetricsHelper extends LazyLogging {
     val server = go(0, port)
     logger.debug(s"You can visit http://localhost:${server.getPort}/ to check the metrics.")
     server
+  }
+
+  /**
+    * In order to get JMX metrics exported to Prometheus, using the default CollectorRegistry with
+    * DefaultExports.initialize() will not work. Hence, this function creates a special collectorRegistry that collects
+    * the Jmx metrics.
+    * @return A custom CollectorRegistry containing Jmx values
+    */
+  private def createCustomCollectorRegistry = {
+    val collectorRegistry = new CollectorRegistry
+    try
+      new JmxCollector("{}").register(collectorRegistry)
+    catch {
+      case ex: MalformedObjectNameException =>
+        logger.error("Error starting Prometheus Jmx Collector. Prometheus will start but without MBeans and" +
+          "some Jmx metrics.", ex)
+    }
+    // Add the default exports from io.prometheus.client.hotspot.DefaultExports
+    new StandardExports().register(collectorRegistry)
+    new MemoryPoolsExports().register(collectorRegistry)
+    new MemoryAllocationExports().register()
+    new BufferPoolsExports().register(collectorRegistry)
+    new GarbageCollectorExports().register(collectorRegistry)
+    new ThreadExports().register(collectorRegistry)
+    new ClassLoadingExports().register(collectorRegistry)
+    new VersionInfoExports().register(collectorRegistry)
+    collectorRegistry
   }
 
 }
